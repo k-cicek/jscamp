6a221b16913b56ba235536f695ddc0b4cb12c7ae
k-cicek <kevserrcicek@gmail.com>
2021-10-19 12:45:22 +0300

    18 October end of class

---
+++ src/app.js
@@ -0,0 +1,21 @@
+console.log("Merhaba Kodlama.io")
+
+var dolarDun = 9.20
+dolarDun = "Ankara"
+var dolarBugun = "9.30"
+
+console.log(dolarDun)
+
+//camelCasing
+//pascalCasing
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i<konutKredileri.length; i++) {
+ console.log("<li>" +konutKredileri[i]+ "</li>")
+  
+}
+
+console.log("</ul>");
+
+
