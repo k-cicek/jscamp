5c33f7b3c015ef4122feab0306174db52b5345dd
k-cicek <kevserrcicek@gmail.com>
2021-11-27 15:15:44 +0300

    22 November end of class

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import { BaseLogger } from "../crossCuttingConcerns/logging/loger.js";
+import Customer from "../models/customer.js";
 import User from "../models/user.js";
 import UserService from "../services/userService.js";
 
@@ -7,12 +8,31 @@ console.log("User component yüklendi");
 let logger1 = new BaseLogger();
 let userService = new UserService(logger1);
 
-let user1 = new User(1,"Kevser", "Çiçek", "İstanbul");
-let user2 = new User(2,"Ilgın", "Kazdal", "İstanbul");
+let user1 = new User(1, "Kevser", "Çiçek", "İstanbul");
+let user2 = new User(2, "Ilgın", "Kazdal", "İstanbul");
 
 userService.add(user1);
 userService.add(user2);
 
-console.log(userService.list())
-console.log(userService.getById(2))
+/* console.log(userService.list());
+console.log(userService.getById(2)); */
 
+let customer = { id:1, firstName: "Engin" };
+
+//prototyping
+customer.lastName = "Demiroğ";
+
+console.log(customer.lastName);
+
+console.log("----------------");
+
+userService.load();
+
+let customerToAdd = new Customer(1, "Seda", "Yılmaz", "Ankara", "fdgdfg");
+customerToAdd.type = "customer";
+
+userService.add(customerToAdd);
+console.log(userService.customers);
+console.log(userService.employees);
+console.log(userService.errors);
+console.log(userService.getCustomersSorted());
